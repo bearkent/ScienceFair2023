61d1110362a40f5a0b67324ed20c88672fcf225d
David R. (Chip) Kent IV <chipkent@cecropiacapital.com>
2023-02-16 21:17:17 -0700

    Test framework

---
+++ TestRealSound.py
@@ -0,0 +1,46 @@
+
+import unittest
+import RealSound as rs
+
+class TestSound(unittest.TestCase):
+    def test_constructor(self):
+        self.fail()
+        #tets len()
+
+    def test_play(self):
+        self.fail()
+
+    def test_fft(self):
+        self.fail()
+
+    def test_record(self):
+        self.fail()
+
+    def test_read_write(self):
+        self.fail()
+
+    def test_sine(self):
+        self.fail()
+
+
+    def test_xxx(self):
+        self.assertEqual(xxx)
+
+
+class TestFFT(unittest.TestCase):
+
+    def test_fail(self):
+        self.fail()
+
+class TestPowerSpectrum(unittest.TestCase):
+    def test_fail(self):
+        self.fail()
+
+**
+# recordamps
+# meaninverse
+# cubicspline
+# plotspline
+
+if __name__ == '__main__':
+    unittest.main()
