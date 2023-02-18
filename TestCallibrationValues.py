90aade3a0d19ca96798951e19cdb560324f30680
Bear Kent <davidbearkent@gmail.com>
2023-02-18 08:15:09 -0700

    Testing callibration values.

--- TestCallibrationValues.py
+++ TestCallibrationValues.py
@@ -5,6 +5,9 @@ from fix import fix
 vals = np.load('CallibrationValues1.npy')
 xs = np.array(vals[0])
 ys = np.array(vals[1])
+
+ys = abs(ys/len(xs))**2
+
 # ys = rs.meaninverse(ys)
 f = rs.cubicspline(xs, ys)
 rs.plotspline(f)
